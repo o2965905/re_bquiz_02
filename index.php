350d8cca860192c8ffab48b0827afa056df0becd
o2965905 <o2965905@gmail.com>
2022-07-30 17:08:33 +0800

    更換jQuery版本(3.4.1)

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <title>健康促進網</title>
     <link href="./css/css.css" rel="stylesheet" type="text/css">
-    <script src="./js/jquery-1.9.1.min.js"></script>
+    <script src="./js/jquery-3.4.1.min.js"></script>
     <script src="./js/js.js"></script>
 </head>
 
@@ -47,7 +47,7 @@
             </div>
         </div>
         <div id="bottom">
-            本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved
+            本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2022健康促進網社群平台 All Right Reserved
             <br>
             服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
         </div>
